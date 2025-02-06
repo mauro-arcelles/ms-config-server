91cd59969aa6cd9c887f913abd9f67e3415fd643
Adrian Mauro Arcelles Ramirez <mauroar1001@hotmail.com>
2025-02-05 19:11:31 -0500

    first commit

---
+++ src/test/java/com/project1/ms_config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.ms_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
